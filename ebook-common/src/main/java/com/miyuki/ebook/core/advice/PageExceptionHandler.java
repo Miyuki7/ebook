bbce957bc7a07a734a444bd17edb7ff4b5282f8e
Miyuki <yuzh7799@gmail.com>
2023-03-01 21:49:11 +0800

    项目创建

---
+++ ebook-common/src/main/java/com/miyuki/ebook/core/advice/PageExceptionHandler.java
@@ -0,0 +1,11 @@
+package com.miyuki.ebook.core.advice;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: miyuki
+ * @Date: 2023/03
+ * @Description:
+ */
+public class PageExceptionHandler {
+}
